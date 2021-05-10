f554e06b9d34024de3384a34e04970d471a22f1a
Jack DeVries <jdevries3133@gmail.com>
2021-05-10 00:00:00 +0000

    viewingArticle(index) works

--- script.js
+++ script.js
@@ -1,70 +1,46 @@
-const STATE = {
-  haveDoneTheThing: false,
-};
-
-function initToolbarBackground() {
-  // create and add to DOM
-  const background = document.createElement("div");
-  background.id = "toolbarBackground";
-  background.classList.add("toolbar", "toolbar--threeItems");
-  document.body.appendChild(background);
-
-  // insert children
-  for (let i = 0; i < 3; i++) {
-    const el = document.createElement("div");
-    el.id = `toolbarChild${i}`;
-    el.classList.add("toolbar__child");
-    el.classList.add(
-      i === 1 ? "toolbar__child--main" : "toolbar__child--secondary"
-    );
-    background.appendChild(el);
-  }
+/**
+ * Update the DOM depending on whatever article is in view. This removes
+ * the Nth item from dynamicFooter and replaces it with an empty placebolder.
+ *
+ * Also, if null is passed, that means that none of the articles are
+ * currently being viewed, and the dynamicFooter returns to it's original
+ * state.
+ */
+function viewingArticle(articleIndex) {
+  destroyDynamicFooter();
+  createDynamicHeader(articleIndex);
 }
 
 /**
- * As the article scrolls into view, change the styling of the toolbar so
- * that the header for this article will scroll up with the article, and the
- * toolbar will change shape.
+ * If omitIndex, we will leave an empty placeholder for that item in the
+ * new toolbar. Like when we are viewing a given article.
  */
-function toolbarToHeader() {
-  const background = document.getElementById("toolbarBackground");
-  background.classList.remove("toolbar--threeItems");
-  background.classList.add("toolbar--twoItems");
-  document.getElementById("toolbarChild1").remove();
+function createDynamicHeader(omitIndex = null) {
+  const container = document.querySelector("main");
+  const headers = document.querySelectorAll("h1");
+
+  const footerHtml = `
+    ${[...headers]
+      .map((h, i) => {
+        console.log(i, omitIndex);
+        return i === omitIndex
+          ? "<div></div>"
+          : `
+    <button id=${h.innerText + "MenuElement"}>
+      ${h.innerText}
+    </button>`;
+      })
+      .join("")}
+  `;
+  const footer = document.createElement("div");
+  footer.id = "dynamicFooter";
+  footer.classList.add("dynamicFooter");
+  footer.innerHTML = footerHtml;
+  container.appendChild(footer);
 }
 
-function headerToToolbar() {
-  const background = document.getElementById("toolbarBackground");
-  background.classList.remove("toolbar--twoItems");
-  background.classList.add("toolbar--threeItems");
-
-  const target = document.getElementById("toolbarChild0");
-  const newChild = document.createElement("div");
-  newChild.id = "toolbarChild1";
-  newChild.classList.add("toolbar__child--main");
-  target.insertAdjacentElement("afterend", newChild);
+function destroyDynamicFooter() {
+  document.getElementById("dynamicFooter").remove();
 }
 
-function handleIntersect(entries, observer) {
-  entries.forEach((e) => {
-    if (e === undefined) {
-      // an undefined event comes in as soon as the page loads??...
-      return;
-    } else if (e.isIntersecting) {
-      toolbarToHeader();
-    } else if (e.boundingClientRect.y < 0) {
-      // we have scrolled past the article. Do nothing
-      return;
-    } else if (STATE.haveDoneTheThing) {
-      headerToToolbar();
-    }
-  });
-}
-
-const observer = new IntersectionObserver(handleIntersect, {
-  root: null,
-  rootMargin: "0px",
-});
-observer.observe(document.getElementById("articleOne"));
-
-initToolbarBackground();
+document.addEventListener("DOMContentLoaded", () => createDynamicHeader());
